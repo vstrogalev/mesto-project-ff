846a45740b8f10bc0fa81acba3adcb011a2b3065
Viacheslav Strogalev <strogalev@hotmail.com>
2023-10-08 09:18:37 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# Проектная работа Mesto
+#Title
+- Проектная работа Mesto
 
+#Project description
+- Страница для тренировки работы с элементами DOM посредством JS. Создание, удаление карточек
+
+#Used technilogies
+JavaScript
+
+#Link to github repo
+https://github.com/vstrogalev/mesto-project-ff
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,40 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+// функция, которая принимает в аргументах данные одной карточки и функцию-колбэк для удаления, а возвращает подготовленный к выводу элемент карточки
+// клонировать шаблон,
+// установить значения вложенных элементов,
+// добавить к иконке удаления обработчик клика, по которому будет вызван переданный в аргументах колбэк.
+
+function createCard(cardData, deleteCardFunction) {
+  const card = cardTemplate.cloneNode(true);
+
+  card.querySelector('.card__image').src = cardData.link;
+  card.querySelector('.card__image').alt = cardData.name;
+  card.querySelector('.card__title').textContent = cardData.name;
+  card.querySelector('.card__delete-button').addEventListener('click', deleteCardFunction);
+
+  return card;
+}
 
 // @todo: Функция удаления карточки
+// В шаблоне карточек уже добавлена иконка удаления, при клике по ней выбранная карточка должна удаляться со страницы
+function deleteCard(evt) {
+  const card = evt.target.parentElement;
+  card.remove();
+}
 
 // @todo: Вывести карточки на страницу
+// вывод всех карточек из массива на страницу в элемент .places__list
+function renderCards() {
+  initialCards.forEach(card => {
+    placesList.append(createCard(card, deleteCard));
+  });
+}
+
+// отображаем на странице
+renderCards();
